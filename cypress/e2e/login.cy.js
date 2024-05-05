1afa2e1da20f91998c519769f6357ea7e246d8e4
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-05-05 13:54:08 +0700

    add test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,28 @@
 describe('Login application', () => {
     beforeEach(() => {
         cy.open_page()
+        cy.xpath_click('//button[normalize-space(.)="Signin"]')
     })
     it('Login successfully', () => {
-        cy.xpath_click('//button[normalize-space(.)="Signin"]')
         cy.fill_login_info('username', 'password')
         cy.xpath_click(`//*[@type='submit']`)
         cy.open_page()
-        cy.xapth_assert_text(`//*[@id='settingsBox']//i[@class='icon-user']//ancestor::a[@class='dropdown-toggle']`, 'username')
+        cy.xpath_assert_text(`//*[@id='settingsBox']//i[@class='icon-user']//ancestor::a[@class='dropdown-toggle']`, 'username')
+    })
+    it('Login unsuccessfully', () => {
+        cy.fill_login_info('username1', 'password')
+        cy.xpath_click(`//*[@type='submit']`)
+        cy.xpath_existing_tag(`//*[@class='alert alert-error']`)
+        cy.xpath_assert_text(`//*[@class='alert alert-error']`, 'Login and/or password are wrong.')
+    })
+
+    it('Forget password', () => {
+        cy.xpath_click(`//*[text()='Forgot your password ?']`)
+        cy.check_url('forgot-password.html')
+        cy.xpath_assert_text('//h3', 'Forgotten Password')
+        cy.xpath_type(`//input[@name='email']`, 'nqb@gmail.com')
+        cy.xpath_click(`//input[@value='Send Password']`)
+        cy.check_url(`forgotten-password-send.html`)
+        cy.xpath_assert_text(`//*[text()='Forgotten Password']//ancestor::div[@class='offset3 span6']`, `Your password will be sent to the following email: nqb@gmail.com`)
     })
 })
--- cypress/support/support.js
+++ cypress/support/support.js
@@ -5,6 +5,14 @@ Cypress.Commands.add('xpath_click', (locator) => {
 Cypress.Commands.add('xpath_type', (locator, value) => {
     cy.xpath(locator.toString()).type(value.toString())
 })
-Cypress.Commands.add('xapth_assert_text', (locator, text) => {
+Cypress.Commands.add('xpath_assert_text', (locator, text) => {
     cy.xpath(locator.toString()).should('contain', text)
+})
+
+Cypress.Commands.add('xpath_existing_tag', (locator) => {
+    cy.xpath(locator.toString()).should('be.visible')
+})
+
+Cypress.Commands.add('check_url', (value) => {
+    cy.url().should('contain', value.toString())
 })
